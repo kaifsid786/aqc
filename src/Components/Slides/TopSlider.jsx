0816a18f2489afb402bd6320725eefa2f1ef3862
MOHD AQIB <mohdaqib921@gmail.com>
2024-01-12 16:35:15 +0530

    modified banner

--- src/Components/Slides/TopSlider.jsx
+++ src/Components/Slides/TopSlider.jsx
@@ -14,23 +14,28 @@ import "./TopSlider.css";
 import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
 import v1 from "../../images/v1.mp4";
 import v2 from "../../images/v2.mp4";
+import PropertyCard from "../PropertyCard/PropertyCard";
 
 export default function TopSlider() {
   return (
     <>
       <Swiper
         spaceBetween={30}
-        autoplay={{ delay: 7000 }}
-        loop={true}
+        // autoplay={{ delay: 7000 }}
+        // loop={true}
         effect={"fade"}
         navigation={true}
         pagination={{
           clickable: true,
         }}
-        freeMode={true}
+        // freeMode={true}
         modules={[EffectFade, Navigation, Pagination, Autoplay]}
         className="mySwiper"
       >
+        <SwiperSlide>
+          <PropertyCard/>
+          
+        </SwiperSlide>
         <SwiperSlide>
           {/* <img src="https://swiperjs.com/demos/images/nature-1.jpg" /> */}
           <video src={v1} autoPlay muted loop></video>
@@ -49,10 +54,9 @@ through <br /> the cutting edge technologies that drive our food processing.</p>
 through <br /> the cutting edge technologies that drive our food processing.</p>
           </div>
         </SwiperSlide>
+
+        
         {/* <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
           <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
         </SwiperSlide> */}
       </Swiper>
